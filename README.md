2b31874071fed9bd75b25fd9f9a1956a0a491cfb
Cendy <cendyemail@gmail.com>
2024-09-13 14:49:06 +0700

    feat: initial release

---
+++ README.md
@@ -0,0 +1,18 @@
+﻿<br/>
+<div align="center">
+<h3 align="center">Steamtoken</h3>
+<p align="center">
+Get your steam game session token
+</p>
+</div>
+
+## Usage
+
+- Spawn the application with argument `appid username password`
+- Read the console output for the token
+
+## Example
+
+```bash
+$ steamtoken 730 username password
+```
---
+++ steamtoken/Program.cs
@@ -0,0 +1,58 @@
+﻿using SteamKit2;
+
+namespace steamtoken;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var appid = "866020";
+        var username = "qzddwoygxfrom";
+        var password = "ypnbh@6!hdiEK";
+        var steamClient = new SteamClient();
+        var manager = new CallbackManager(steamClient);
+        var steamUser = steamClient.GetHandler<SteamUser>();
+        var steamAuthTicket = steamClient.GetHandler<SteamAuthTicket>();
+        var isRunning = true;
+        byte[]? token = null;
+
+        manager.Subscribe<SteamClient.ConnectedCallback>(callback =>
+        {
+            steamUser.LogOn(new SteamUser.LogOnDetails
+            {
+                Username = username,
+                Password = password
+            });
+        });
+
+        manager.Subscribe<SteamUser.LoggedOnCallback>(callback =>
+        {
+            if (callback.Result != EResult.OK)
+            {
+                isRunning = false;
+                Console.WriteLine("Failed to log in to steam");
+            }
+
+            token = steamAuthTicket.GetAuthSessionTicket((uint)int.Parse(appid)).Result.Ticket.ToArray();
+            isRunning = false;
+        });
+
+        steamClient.Connect();
+
+        while (isRunning)
+        {
+            manager.RunWaitCallbacks(TimeSpan.FromSeconds(1));
+        }
+
+        steamUser.LogOff();
+        if (token != null)
+        {
+            string hexString = BitConverter.ToString(token).Replace("-", " ");
+            Console.WriteLine(hexString);
+        }
+        else
+        {
+            Console.WriteLine("No token received");
+        }
+    }
+}
